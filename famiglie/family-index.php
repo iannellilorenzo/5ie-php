1cf86cd600e2935666a292043d22a96fe9c25c27
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-09-24 22:47:52 +0200

    file used to fix php formatting in vscode

--- famiglie/family-index.php
+++ famiglie/family-index.php
@@ -9,12 +9,14 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionale famiglie</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="container mt-5">
         <div class="row">
@@ -22,7 +24,7 @@
                 <div class="card">
                     <img src="img/female.jpg" class="card-img-top" alt="Card Image">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $first_person->cognome." ".$first_person->nome; ?></h5>
+                        <h5 class="card-title"><?php echo $first_person->cognome . " " . $first_person->nome; ?></h5>
                         <p class="card-text"></p>
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#infoModal">Tell me more</button>
                     </div>
@@ -63,9 +65,9 @@
                 </div>
                 <div class="modal-body">
                     <?php
-                        foreach ($first_person as $field => $value) {
-                            echo "<p><strong>$field:</strong> $value</p>";
-                        }
+                    foreach ($first_person as $field => $value) {
+                        echo "<p><strong>$field:</strong> $value</p>";
+                    }
                     ?>
                 </div>
                 <div class="modal-footer">
@@ -79,4 +81,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
