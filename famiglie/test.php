8672fa5ed04982ce1268b331398ab08bc644d8ad
iannellilorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-09-24 11:38:54 +0200

    search by id now works. TODO: search by tax id code

--- famiglie/test.php
+++ famiglie/test.php
@@ -2,7 +2,8 @@
     $families = file_get_contents("Famiglie.json");
     $db = json_decode($families);
     //var_dump($db);
-    $first_person = $db[0];
+    $index = $_GET["n"];
+    $first_person = $db[$index];
 ?>
 
 
@@ -11,8 +12,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bootstrap 5 Card Example</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <title>Gestionale famiglie</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
@@ -21,15 +22,9 @@
                 <div class="card">
                     <img src="img/female.jpg" class="card-img-top" alt="Card Image">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $first_person->cognome." ".$first_person->nome ?></h5>
-                        <p class="card-text">
-                            <?php
-                                foreach ($first_person as $campo) {
-                                    echo "$campo<br>";
-                                }
-                            ?>
-                        </p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"><?php echo $first_person->cognome." ".$first_person->nome; ?></h5>
+                        <p class="card-text"></p>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#infoModal">Tell me more</button>
                     </div>
                 </div>
             </div>
@@ -56,8 +51,32 @@
         </div>
     </div>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <!-- Modal -->
+    <div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel<?php echo $first_person->id; ?>" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="infoModalLabel<?php echo $first_person->id; ?>">Family Member Information</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?php
+                        foreach ($first_person as $field => $value) {
+                            echo "<p><strong>$field:</strong> $value</p>";
+                        }
+                    ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-
