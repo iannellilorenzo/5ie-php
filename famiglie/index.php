1672eb474dfa294fee07b13d6b78b99f81ca346a
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-09-25 20:51:02 +0200

    done the dropdown menu for provinces

--- famiglie/index.php
+++ famiglie/index.php
@@ -11,6 +11,119 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
+<?php
+    $provinces = [
+        "AG" => "Agrigento",
+        "AL" => "Alessandria",
+        "AN" => "Ancona",
+        "AO" => "Aosta",
+        "AR" => "Arezzo",
+        "AP" => "Ascoli Piceno",
+        "AT" => "Asti",
+        "AV" => "Avellino",
+        "BA" => "Bari",
+        "BT" => "Barletta-Andria-Trani",
+        "BL" => "Belluno",
+        "BN" => "Benevento",
+        "BG" => "Bergamo",
+        "BI" => "Biella",
+        "BO" => "Bologna",
+        "BZ" => "Bolzano",
+        "BS" => "Brescia",
+        "BR" => "Brindisi",
+        "CA" => "Cagliari",
+        "CL" => "Caltanissetta",
+        "CB" => "Campobasso",
+        "CI" => "Carbonia-Iglesias",
+        "CE" => "Caserta",
+        "CT" => "Catania",
+        "CZ" => "Catanzaro",
+        "CH" => "Chieti",
+        "CO" => "Como",
+        "CS" => "Cosenza",
+        "CR" => "Cremona",
+        "KR" => "Crotone",
+        "CN" => "Cuneo",
+        "EN" => "Enna",
+        "FM" => "Fermo",
+        "FE" => "Ferrara",
+        "FI" => "Florence",
+        "FG" => "Foggia",
+        "FC" => "Forlì-Cesena",
+        "FR" => "Frosinone",
+        "GE" => "Genoa",
+        "GO" => "Gorizia",
+        "GR" => "Grosseto",
+        "IM" => "Imperia",
+        "IS" => "Isernia",
+        "SP" => "La Spezia",
+        "AQ" => "L'Aquila",
+        "LT" => "Latina",
+        "LE" => "Lecce",
+        "LC" => "Lecco",
+        "LI" => "Livorno",
+        "LO" => "Lodi",
+        "LU" => "Lucca",
+        "MC" => "Macerata",
+        "MN" => "Mantua",
+        "MS" => "Massa and Carrara",
+        "MT" => "Matera",
+        "VS" => "Medio Campidano",
+        "ME" => "Messina",
+        "MI" => "Milan",
+        "MO" => "Modena",
+        "MB" => "Monza and Brianza",
+        "NA" => "Naples",
+        "NO" => "Novara",
+        "NU" => "Nuoro",
+        "OR" => "Oristano",
+        "PD" => "Padua",
+        "PA" => "Palermo",
+        "PR" => "Parma",
+        "PV" => "Pavia",
+        "PG" => "Perugia",
+        "PU" => "Pesaro and Urbino",
+        "PE" => "Pescara",
+        "PC" => "Piacenza",
+        "PI" => "Pisa",
+        "PT" => "Pistoia",
+        "PN" => "Pordenone",
+        "PZ" => "Potenza",
+        "PO" => "Prato",
+        "RG" => "Ragusa",
+        "RA" => "Ravenna",
+        "RC" => "Reggio Calabria",
+        "RE" => "Reggio Emilia",
+        "RI" => "Rieti",
+        "RN" => "Rimini",
+        "RM" => "Rome",
+        "RO" => "Rovigo",
+        "SA" => "Salerno",
+        "SS" => "Sassari",
+        "SV" => "Savona",
+        "SI" => "Siena",
+        "SR" => "Syracuse",
+        "SO" => "Sondrio",
+        "TA" => "Taranto",
+        "TE" => "Teramo",
+        "TR" => "Terni",
+        "TO" => "Turin",
+        "TP" => "Trapani",
+        "TN" => "Trento",
+        "TV" => "Treviso",
+        "TS" => "Trieste",
+        "UD" => "Udine",
+        "VA" => "Varese",
+        "VE" => "Venice",
+        "VB" => "Verbano-Cusio-Ossola",
+        "VC" => "Vercelli",
+        "VR" => "Verona",
+        "VV" => "Vibo Valentia",
+        "VI" => "Vicenza",
+        "VT" => "Viterbo",
+    ];
+?>
+
 <body>
     <div class="container mt-5">
         <h1 class="alert alert-info">Gestionale Famiglie</h1>
@@ -57,7 +170,13 @@
         <form action="province.php" method="GET">
             <div class="form-group">
                 <label for="province">Province</label>
-                <input type="text" class="form-control" id="province" name="prov" placeholder="Enter province" required>
+                <select class="form-control" id="province" name="prov" required>
+                    <?php
+                        foreach ($provinces as $code => $name) {
+                            echo "<option value='{$code}'>{$code} - {$name}</option>";
+                        }
+                    ?>
+                </select>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
