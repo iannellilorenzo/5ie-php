36d0906e654a6b0c9b495888776ba1dea5d94abb
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-12-17 22:45:49 +0100

    changes to the db

--- cs/account-manager/assets/database_info/account_manager.sql
+++ cs/account-manager/assets/database_info/account_manager.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 09, 2024 alle 11:40
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.2.4
+-- Generation Time: Dec 17, 2024 at 10:45 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `accounts`
+-- Table structure for table `accounts`
 --
 
 CREATE TABLE `accounts` (
@@ -39,7 +39,7 @@ CREATE TABLE `accounts` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `roles`
+-- Table structure for table `roles`
 --
 
 CREATE TABLE `roles` (
@@ -47,10 +47,18 @@ CREATE TABLE `roles` (
   `role` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'user');
+
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `statuses`
+-- Table structure for table `statuses`
 --
 
 CREATE TABLE `statuses` (
@@ -58,18 +66,27 @@ CREATE TABLE `statuses` (
   `status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `status`) VALUES
+(1, 'active'),
+(2, 'inactive'),
+(3, 'suspended');
+
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
-  `first_name` varchar(35) NOT NULL,
-  `last_name` varchar(35) NOT NULL,
+  `first_name` varchar(35) DEFAULT NULL,
+  `last_name` varchar(35) DEFAULT NULL,
   `password_hash` varchar(50) NOT NULL,
-  `phone_number` varchar(20) NOT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `status_id` int(11) NOT NULL,
@@ -77,70 +94,78 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indici per le tabelle scaricate
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `first_name`, `last_name`, `password_hash`, `phone_number`, `created_at`, `updated_at`, `status_id`, `role_id`) VALUES
+('Lockr!iannox', NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$RVRrYXZHTHouLjl1bnl', '+12 345 678 9012', '2024-12-17 20:36:05', '2024-12-17 20:36:05', 1, 1),
+('Lockr!iannox2', NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$Wm5lOS9QWmNGLlNxWi5', '+12 345 678 9012', '2024-12-17 21:22:56', '2024-12-17 21:22:56', 1, 1);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `accounts`
+-- Indexes for table `accounts`
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `users-accounts` (`user_id`);
 
 --
--- Indici per le tabelle `roles`
+-- Indexes for table `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indici per le tabelle `statuses`
+-- Indexes for table `statuses`
 --
 ALTER TABLE `statuses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indici per le tabelle `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`email`),
+  ADD PRIMARY KEY (`username`),
   ADD KEY `role-user` (`role_id`),
   ADD KEY `status-user` (`status_id`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT per la tabella `accounts`
+-- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT per la tabella `roles`
+-- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT per la tabella `statuses`
+-- AUTO_INCREMENT for table `statuses`
 --
 ALTER TABLE `statuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Limiti per le tabelle scaricate
+-- Constraints for dumped tables
 --
 
 --
--- Limiti per la tabella `accounts`
+-- Constraints for table `accounts`
 --
 ALTER TABLE `accounts`
-  ADD CONSTRAINT `users-accounts` FOREIGN KEY (`user_id`) REFERENCES `users` (`email`);
+  ADD CONSTRAINT `users-accounts` FOREIGN KEY (`user_id`) REFERENCES `users` (`username`);
 
 --
--- Limiti per la tabella `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `role-user` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
