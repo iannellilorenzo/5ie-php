be5fd5839b531dc09977649e827ef575adac4a9c
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-12-18 22:42:03 +0100

    another version of the db just to be sure

--- cs/account-manager/assets/database_info/account_manager.sql
+++ cs/account-manager/assets/database_info/account_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2024 at 07:15 PM
+-- Generation Time: Dec 18, 2024 at 10:41 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,7 +31,7 @@ CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
   `username` varchar(35) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `password` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `user_reference` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -86,13 +86,21 @@ CREATE TABLE `users` (
   `username` varchar(30) NOT NULL,
   `first_name` varchar(35) DEFAULT NULL,
   `last_name` varchar(35) DEFAULT NULL,
-  `password_hash` varchar(50) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `status_id` int(11) NOT NULL,
   `role_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`email`, `username`, `first_name`, `last_name`, `password_hash`, `phone_number`, `created_at`, `updated_at`, `status_id`, `role_id`) VALUES
+('iannelli.lorenzo2006@gmail.com', 'iannelli2', NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$cXR6eWFHUE5LT3lYckxVMw$Glk9wOjsqWCWRnHBcRhku//eqRfwldh9IvHQuVRid+Q', '+12 345 678 9012', '2024-12-18 21:11:29', '2024-12-18 21:11:29', 1, 2),
+('iannelli@lockr.com', 'iannelli', NULL, NULL, '$argon2id$v=19$m=65536,t=4,p=1$clc1cE40U2p5T0tFWnp1cw$mVxlGCE82Q480Nt8+2nNIkr9SmYfcJWjnyCoHP9gXsk', '+39 123 456 7890', '2024-12-18 18:49:23', '2024-12-18 18:49:23', 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -122,6 +130,7 @@ ALTER TABLE `statuses`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD KEY `role-user` (`role_id`),
   ADD KEY `status-user` (`status_id`);
 
